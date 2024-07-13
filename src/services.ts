f5204711d0303f68ebe8e6b798dfe65323a5812d
Arnau B. <arnau_b17@hotmail.com>
2024-07-13 20:17:34 +0200

    refactor Services & names

--- src/services.ts
+++ src/services.ts
@@ -1,14 +1,29 @@
+import { language } from "./const";
 import { SeriesListRequest } from "./interfaces";
 
-async function getSeries(): Promise<SeriesListRequest[]> {
+
+async function getSeries(typeList: string): Promise<SeriesListRequest[]> {
+   
+    const key = import.meta.env.VITE_TMDB_API_KEY
+    const url = import.meta.env.VITE_TMDB_BASE_URL
 
     try {
-        const url = `https://api.themoviedb.org/3/tv/popular?language=en-US&page=1&api_key=c6aeee577586ba38e487b74dfede5deb`;
-        const response = await fetch(url);
+        const basicUrl = `${url}/tv/${typeList}`;
+
+        const params = new URLSearchParams({
+            page: "1",
+            language: language,
+            api_key: key,
+        });
+
+        const completeUrl = `${basicUrl}?${params.toString()}`;
+        const response = await fetch(completeUrl);
         const data = await response.json();
+
         return data.results;
+
     } catch (e: any) {
-        return e;
+        console.log(e);
     }
 }
 
