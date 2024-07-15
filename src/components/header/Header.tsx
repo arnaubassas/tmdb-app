90c1ad053665451c6f3150ab846c78780e71d112
Arnau Bassas Cordoba <112616397+arnaubassas@users.noreply.github.com>
2024-07-15 18:55:18 +0200

    add globals var scss and refactor responsive design (#4)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,10 +5,16 @@ const Header = () => {
     <div className="header">
       <div className="header__logo">TMDB APP</div>
       <div className="header__seriesOptions">
-        <Link to="airing_today">Airing Today</Link>
-        <Link to="on_the_air">On The Air</Link>
         <Link to="popular">Popular</Link>
+      </div>
+      <div className="header__seriesOptions">
         <Link to="top_rated">Top Rated</Link>
+      </div>
+      <div className="header__seriesOptions">
+        <Link to="airing_today">Airing Today</Link>
+      </div>
+      <div className="header__seriesOptions">
+        <Link to="on_the_air">On The Air</Link>
       </div>
       <div className="header__user">
         <div className="header__user__photo"></div>
