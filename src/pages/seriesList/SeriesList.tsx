65f406585332854c9bda816aa972d69003be5e68
Arnau B. <arnau_b17@hotmail.com>
2024-07-16 12:38:12 +0200

    refactor deign serieList

--- src/pages/seriesList/SeriesList.tsx
+++ src/pages/seriesList/SeriesList.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getSeries } from "../../services";
+import { getSeries, SeriesType } from "../../services";
 import { SeriesListRequest } from "../../interfaces";
 
 import "./SeriesList.scss";
@@ -13,7 +13,7 @@ const SeriesList = () => {
   const [series, setSeries] = useState<SeriesListRequest[]>();
   const [error, setError] = useState(false);
 
-  const { list } = useParams();
+  const { list } = useParams<{ list: SeriesType }>();
 
   useEffect(() => {
     if (list) {
@@ -29,20 +29,23 @@ const SeriesList = () => {
   if (!series) return <Loading />;
 
   return (
-    <>
-      <div className="seriesList">
+    <div className="seriesListPage">
+      <div className="seriesListPage__seriesList">
         {series.map((serie) => (
-          <Card key={serie.id}>
-            <Link to={`/serie/${serie.id}`} className="seriesList__link">
+          <Card key={serie.id} size="small">
+            <Link
+              to={`/serie/${serie.id}`}
+              className="seriesListPage__seriesList__link"
+            >
               <img
                 src={`https://media.themoviedb.org/t/p/w440_and_h660_face/${serie.poster_path}`}
                 alt="poster"
               />
-              <div className="seriesList__link__informationSerie">
-                <div className="seriesList__link__informationSerie__title">
+              <div className="seriesListPage__seriesList__link__informationSerie">
+                <div className="seriesListPage__seriesList__link__informationSerie__title">
                   {serie.name}
                 </div>
-                <div className="seriesList__link__informationSerie__date">
+                <div className="seriesListPage__seriesList__link__informationSerie__date">
                   {dateModify(serie.first_air_date)}
                 </div>
               </div>
@@ -50,7 +53,7 @@ const SeriesList = () => {
           </Card>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
